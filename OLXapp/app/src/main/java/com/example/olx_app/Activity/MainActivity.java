e0fab4eee458cbf5ff381836128406de39d77bbf
CelsoTNeto <cteixeiraneto@hotmail.com>
2019-10-23 22:37:01 -0300

    Alterando a tela de cadastro

--- OLXapp/app/src/main/java/com/example/olx_app/Activity/MainActivity.java
+++ OLXapp/app/src/main/java/com/example/olx_app/Activity/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -16,10 +17,20 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
+import com.example.olx_app.config.CPFauth;
+
+
 public class MainActivity extends AppCompatActivity {
 
-    private EditText campoNome, campoEmail, campoSenha, campoEndereco, campoTelefone, campoCPF;
+
+
+
+
+    private EditText campoNome, campoEmail, campoSenha ,campoCPF;
     private FirebaseAuth autenticacao;
+    private boolean CPFauth;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,9 +38,8 @@ public class MainActivity extends AppCompatActivity {
         campoNome = findViewById(R.id.campoNome);
         campoEmail = findViewById(R.id.campoEmail);
         campoSenha = findViewById(R.id.campoSenha);
-        //campoEndereco = findViewById(R.id.campoEndereco);
-        //campoTelefone = findViewById(R.id.campoTEL);
         campoCPF = findViewById(R.id.campoCPF);
+
 
 
     }
@@ -47,24 +57,31 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    Button btnCadastrar = (Button) findViewById(R.id.btnCadastrar);
     public void validarUsuario(View view) {
-        String textoNome = campoNome.getText().toString();
-        String textoEmail = campoEmail.getText().toString();
-        String textoSenha = campoSenha.getText().toString();
-        //String textoEndereco = campoEndereco.getText().toString();
-       // String textoTelefone = campoTelefone.getText().toString();
-        String textoCPF = campoCPF.getText().toString();
 
-        if (!textoNome.isEmpty() &&
-                !textoEmail.isEmpty() &&
-                !textoSenha.isEmpty() &&
-               // !textoEndereco.isEmpty() &&
-               // !textoTelefone.isEmpty() &&
-            !textoCPF.isEmpty()) {
 
-        }
-        else {
-            Toast.makeText(MainActivity.this, "preencha todos os dados !", Toast.LENGTH_SHORT).show();
-        }
+
+        btnCadastrar.setOnClickListener(new View.OnClickListener() {
+
+            final String textoNome = campoNome.getText().toString();
+            final String textoEmail = campoEmail.getText().toString();
+            final String textoSenha = campoSenha.getText().toString();
+            //String textoEndereco = campoEndereco.getText().toString();
+            // String textoTelefone = campoTelefone.getText().toString();
+            final String textoCPF = campoCPF.getText().toString();
+            public void onClick(View v) {
+                if (!textoNome.isEmpty() &&
+                        !textoEmail.isEmpty() &&
+                        !textoSenha.isEmpty() &&
+                        !textoCPF.isEmpty() && CPFauth == true) {
+                    Toast.makeText(MainActivity.this, "Cadastro realizado com sucesso " , Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    Toast.makeText(MainActivity.this, "preencha todos os dados !", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
     }
 }
